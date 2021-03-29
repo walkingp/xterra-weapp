1293a499be3a317b7c8a46d6118da8ce54bd792f
Hong.Jian.Michael.Wang <Hong.Jian.Michael.Wang@dnv.com>
2021-03-29 21:57:48 +0800

    原始比例

--- miniprogram/config/config.js
+++ miniprogram/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   env: 'xterra-c2969f',
   appId: 'wx491fb14c7948a753',
-  version: 1,
+  version: 2,
   appTitle: 'XTERRA',
   appDescription: 'XTERRA官方小程序',
   mapKey: 'YOWBZ-MK263-F7G3H-YWGGY-RGHKQ-ACFNH',
